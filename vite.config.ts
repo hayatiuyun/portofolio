563b08123d80f88cf988d13e0c950cde921de904
hayatiuyun <hayatiuyun@gmail.com>
2024-06-20 13:06:17 +0700

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hayati-uyun-frontend-dev-portfolio/",
+  base: "/portofolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
