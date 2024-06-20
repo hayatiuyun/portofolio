7289cbba13add6616a0d29996acb6d5a4c2a2233
hayatiuyun <hayatiuyun@gmail.com>
2024-06-20 13:27:13 +0700

    fix image and translations

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -7,11 +7,11 @@ const Footer = () => {
     <footer className=" w-full bg-bluish px-3 dark:bg-[rgb(15,22,36)] md:px-8  lg:px-32">
       <div className="my-auto mx-auto w-full max-w-[1350px] md:pt-12 2xl:pt-12 pb-4 flex flex-col items-center justify-center">
         <div>
-          <img src="/images/striking.png" alt="logo" className="w-20 h-20" />
+          <img src={t("logo")} alt="logo" className="w-20 h-20" />
         </div>
         <p className="text-center">
-          Crafted with React and TailwindCSS. Hosted on Vercel. <br />Developed by Hayati Qurrotul Uyun
-          <p className="pb-2">{2024} &copy; hayatiuyun-vercel.com</p>
+          Crafted with React and TailwindCSS. Hosted on Github. <br />Developed by Hayati Qurrotul Uyun
+          <p className="pb-2">{2024} &copy; hayatiuyun.github.io</p>
 
         </p>
       <div className="flex justify-center gap-5">
